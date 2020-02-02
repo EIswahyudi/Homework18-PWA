f3e73ef6c4ea39d5a986a9b66a67555a9b2f9e70
Evan Iswahyudi <evan_iswahyu@yahoo.com>
2020-02-01 23:23:45 -0800

    make it better

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-const app = express();
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
